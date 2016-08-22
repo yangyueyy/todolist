e5653159749b30566055611d2bf66e47a7963f80
yangyue <m18710434142_1@163.com>
2016-08-22 12:21:49 +0800

    file init

---
+++ public/src/containers/app.js
@@ -0,0 +1,13 @@
+import React, {Component} from "react";
+import {render} from 'react-dom'
+class App extends Component{
+    render() {
+        return (
+            <div>
+               Hello!
+            </div>
+        )
+    }
+}
+
+export default App;
